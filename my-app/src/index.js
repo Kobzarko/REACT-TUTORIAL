260bcab25ecd235802a066b2eeaa5a54c3fa1bd6
Kobzarko <evgewi4@gmail.com>
2021-04-30 00:04:44 +0300

    JSX syntax add-on

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,23 +7,40 @@ import "./index.css";
 // const elem = <h2>Hello world</h2>;
 // const elem2 = React.createElement("h3", null, "Hello team!");
 
+// все атрибуты писать в camelCase
+
 const Header = () => {
   return <h2>Hello my friend</h2>;
 };
 
 const Field = () => {
-  return <input type="text" placeholder="Type here" />;
+  const holder = "Enter here";
+  const styleField = {
+    width: "300px",
+  };
+
+  return (
+    <input
+      style={styleField}
+      type="text"
+      placeholder={holder}
+      autoComplete=""
+      className="first"
+      htmlFor=""
+    />
+  );
 };
 
 const Btn = () => {
   const text = "Log in";
+  const logged = true;
   // const Res = () => {
   //   return "Log in, please";
   // };
   // const p = <p>Log in</p>;
   // Мы не можем помещать объекты внутрь наших реакт элементов
 
-  return <button>{text}</button>;
+  return <button>{logged ? null : text}</button>;
 };
 
 const App = () => {
