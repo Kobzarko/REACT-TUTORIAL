6247499d323bc0f83ad51f6442e2c50b2ce704dd
Kobzarko <evgewi4@gmail.com>
2021-04-29 23:52:36 +0300

    JSX syntax

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const elem = <h2>Hello world</h2>;
+// const elem2 = React.createElement("h3", null, "Hello team!");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Header = () => {
+  return <h2>Hello my friend</h2>;
+};
+
+const Field = () => {
+  return <input type="text" placeholder="Type here" />;
+};
+
+const Btn = () => {
+  const text = "Log in";
+  // const Res = () => {
+  //   return "Log in, please";
+  // };
+  // const p = <p>Log in</p>;
+  // Мы не можем помещать объекты внутрь наших реакт элементов
+
+  return <button>{text}</button>;
+};
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Field />
+      <Btn />
+    </div>
+  );
+};
+
+// ReactDOM.render(elem, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
