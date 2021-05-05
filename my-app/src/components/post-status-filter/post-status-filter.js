6c95c9b1d277ad6d42e0b48f9329a1f7d979c2fc
Kobzarko <evgewi4@gmail.com>
2021-05-05 23:54:37 +0300

    add button style

--- my-app/src/components/post-status-filter/post-status-filter.js
+++ my-app/src/components/post-status-filter/post-status-filter.js
@@ -5,8 +5,12 @@ import "./post-status-filter.css";
 const PostStatusFilter = () => {
   return (
     <div className="btn-group">
-      <button />
-      <button />
+      <button type="button" className="btn btn-info">
+        ALL
+      </button>
+      <button type="button" className="btn btn-outline-secondary">
+        LIKE
+      </button>
     </div>
   );
 };
