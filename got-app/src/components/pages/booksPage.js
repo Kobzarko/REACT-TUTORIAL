f5e4b5a441ebfa3a0d7f72a7544c8a87dd36c6b6
Kobzarko <evgewi4@gmail.com>
2021-06-04 23:19:11 +0300

    booksPage

--- got-app/src/components/pages/booksPage.js
+++ got-app/src/components/pages/booksPage.js
@@ -1,6 +1,43 @@
 import React, { Component } from "react";
 import ItemList from "../itemList";
-import ItemDetails, { Field } from "../itemDetails";
 import ErrorMessage from "../errorMessage";
 import gotService from "../../services/gotService";
-import RowBlock from "../rowBlock";
+import { withRouter } from "react-router-dom";
+
+export class BooksPage extends Component {
+  gotService = new gotService();
+
+  state = {
+    selectedBook: null,
+    error: false,
+  };
+
+  onItemSelected = (id) => {
+    this.setState({
+      selectedBook: id,
+    });
+  };
+
+  componentDidCatch() {
+    this.setState({
+      error: true,
+    });
+  }
+  render() {
+    if (this.state.error) {
+      return <ErrorMessage />;
+    }
+
+    return (
+      <ItemList
+        onItemSelected={(itemId) => {
+          this.props.history.push(itemId);
+        }}
+        getData={this.gotService.getAllBooks}
+        renderItem={({ name }) => name}
+      />
+    );
+  }
+}
+
+export default withRouter(BooksPage);
